03462379126d2861f9bb26a546af06a757d24d51
brb484ul <pppphp2800@gmail.com>
2025-11-24 16:20:20 +0300

    Scattering data from a rank 0 to others ranks

--- tasks/khruev_a_min_elem_vec/mpi/src/ops_mpi.cpp
+++ tasks/khruev_a_min_elem_vec/mpi/src/ops_mpi.cpp
@@ -4,8 +4,6 @@
 
 #include <algorithm>
 #include <climits>
-#include <cstddef>
-#include <utility>
 #include <vector>
 
 #include "khruev_a_min_elem_vec/common/include/common.hpp"
@@ -27,37 +25,45 @@ bool KhruevAMinElemVecMPI::PreProcessingImpl() {
 }
 
 bool KhruevAMinElemVecMPI::RunImpl() {
-  if (GetInput().empty()) {
+  const auto &input = GetInput();
+  if (input.empty()) {
     GetOutput() = INT_MAX;
     return true;
   }
+
   int rank = 0;
   int size = 0;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
-  int global_min = 0;
-  int local_min = 0;
 
-  size_t vec_size = GetInput().size();
+  int n = static_cast<int>(input.size());
+  int int_part = n / size;
+  int remainder = n % size;
 
-  size_t int_part = vec_size / size;
-  size_t remainder = vec_size % size;
+  std::vector<int> sendcounts(size);
+  std::vector<int> displs(size);
 
-  size_t start = (int_part * rank) + std::min(static_cast<size_t>(rank), remainder);
-  size_t end = start + int_part - 1 + (std::cmp_less(static_cast<size_t>(rank), remainder) ? 1 : 0);
-  local_min = INT_MAX;
-  if (start < vec_size) {
-    local_min = GetInput()[start];
-    for (size_t i = start; i <= end; i++) {
-      local_min = std::min(local_min, GetInput()[i]);
-    }
+  for (int i = 0; i < size; ++i) {
+    sendcounts[i] = int_part + (i < remainder ? 1 : 0);
+    displs[i] = (i == 0 ? 0 : displs[i - 1] + sendcounts[i - 1]);
   }
 
-  MPI_Reduce(&local_min, &global_min, 1, MPI_INT, MPI_MIN, 0, MPI_COMM_WORLD);
+  std::vector<int> local_chunk(sendcounts[rank] > 0 ? sendcounts[rank] : 1);
+  MPI_Scatterv(input.data(), sendcounts.data(), displs.data(), MPI_INT,
+               sendcounts[rank] > 0 ? local_chunk.data() : nullptr, sendcounts[rank], MPI_INT, 0, MPI_COMM_WORLD);
 
+  int local_min = INT_MAX;
+  if (sendcounts[rank] > 0) {
+    local_min = *std::min_element(local_chunk.begin(), local_chunk.begin() + sendcounts[rank]);
+  }
+
+  int global_min = 0;
+
+  MPI_Reduce(&local_min, &global_min, 1, MPI_INT, MPI_MIN, 0, MPI_COMM_WORLD);
   MPI_Bcast(&global_min, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
   GetOutput() = global_min;
+
   return true;
 }
 
