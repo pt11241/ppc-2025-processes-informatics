f6b2effd312c447e56a643bc299c120824046497
brb484ul <pppphp2800@gmail.com>
2025-12-31 00:23:48 +0300

    fix func tests ://///

--- tasks/khruev_a_gauss_jordan/tests/functional/main.cpp
+++ tasks/khruev_a_gauss_jordan/tests/functional/main.cpp
@@ -1,59 +1,127 @@
 #include <gtest/gtest.h>
 
+#include <array>
 #include <cmath>
 #include <cstddef>
+#include <string>
+#include <tuple>
 #include <vector>
 
 #include "khruev_a_gauss_jordan/common/include/common.hpp"
 #include "khruev_a_gauss_jordan/mpi/include/ops_mpi.hpp"
 #include "khruev_a_gauss_jordan/seq/include/ops_seq.hpp"
-#include "util/include/perf_test_util.hpp"
+#include "util/include/func_test_util.hpp"
+#include "util/include/util.hpp"
 
 namespace khruev_a_gauss_jordan {
 
-class KhruevARunPerfTestsProcesses : public ppc::util::BaseRunPerfTests<InType, OutType> {
-  const int size_ = 100;
-  InType input_data_;
-
-  void SetUp() override {
-    int n = size_;
-    int m = size_;
-    input_data_ = InType(static_cast<size_t>(n), std::vector<double>(static_cast<size_t>(m + 1)));
-
-    for (int i = 0; i < n; i++) {
-      double sum = 0;
-      for (int j = 0; j < m; j++) {
-        if (i == j) {
-          input_data_[i][j] = 10.0 + ((i % 100) / 10.0);
-        } else {
-          input_data_[i][j] = (i + j) % 10 / 10.0;
-        }
-        sum += std::abs(input_data_[i][j]);
-      }
-      input_data_[i][m] = sum;
-    }
+class KhruevARunFuncTestsProcesses
+    : public ppc::util::BaseRunFuncTests<InType, OutType, TestType> {
+ public:
+  static std::string PrintTestParam(const TestType& param) {
+    return std::get<0>(param);
   }
 
-  bool CheckTestOutputData(OutType &output_data) final {
-    return !output_data.empty();
+ protected:
+  void SetUp() override {
+    TestType test_data =
+        std::get<static_cast<std::size_t>(
+            ppc::util::GTestParamIndex::kTestParams)>(GetParam());
+    input_matrix_ = std::get<1>(test_data);
+    expected_result_ = std::get<2>(test_data);
+  }
+
+  bool CheckTestOutputData(OutType& output) final {
+    if (expected_result_.size() != output.size()) {
+      return false;
+    }
+    for (size_t i = 0; i < expected_result_.size(); ++i) {
+      if (std::abs(expected_result_[i] - output[i]) > 1e-6) {
+        return false;
+      }
+    }
+    return true;
   }
 
   InType GetTestInputData() final {
-    return input_data_;
+    return input_matrix_;
   }
+
+ private:
+  InType input_matrix_;
+  OutType expected_result_;
 };
 
-TEST_P(KhruevARunPerfTestsProcesses, RunPerfModes) {
+namespace {
+
+TEST_P(KhruevARunFuncTestsProcesses, GaussJordan) {
   ExecuteTest(GetParam());
 }
 
-const auto kAllPerfTasks = ppc::util::MakeAllPerfTasks<InType, KhruevAGaussJordanMPI, KhruevAGaussJordanSEQ>(
-    PPC_SETTINGS_khruev_a_gauss_jordan);
+/* Уменьшенный и изменённый набор тестов */
+const std::array<TestType, 7> kTestParam = {
+    std::make_tuple(
+        "simple_2x2",
+        InType{{3, 1, 7},
+               {1, 2, 5}},
+        OutType{1.0, 2.0}),
 
-const auto kGtestValues = ppc::util::TupleToGTestValues(kAllPerfTasks);
+    std::make_tuple(
+        "row_swap_case",
+        InType{{0, 4, 8},
+               {2, 1, 5}},
+        OutType{2.0, 2.0}),
 
-const auto kPerfTestName = KhruevARunPerfTestsProcesses::CustomPerfTestName;
+    std::make_tuple(
+        "identity_3x3",
+        InType{{1, 0, 0, 4},
+               {0, 1, 0, 5},
+               {0, 0, 1, 6}},
+        OutType{4.0, 5.0, 6.0}),
 
-INSTANTIATE_TEST_SUITE_P(RunModeTests, KhruevARunPerfTestsProcesses, kGtestValues, kPerfTestName);
+    std::make_tuple(
+        "diagonal_3x3",
+        InType{{5, 0, 0, 10},
+               {0, 2, 0, 6},
+               {0, 0, 4, 8}},
+        OutType{2.0, 3.0, 2.0}),
 
+    std::make_tuple(
+        "fractional_coeffs",
+        InType{{0.2, 0.4, 1.2},
+               {0.6, 0.2, 1.0}},
+        OutType{1.0, 2.0}),
+
+    std::make_tuple(
+        "single_equation",
+        InType{{5, 20}},
+        OutType{4.0}),
+
+    std::make_tuple(
+        "upper_triangular",
+        InType{{2, 1, 1, 9},
+               {0, 1, 2, 8},
+               {0, 0, 2, 4}},
+        OutType{2.0, 2.0, 2.0}),
+};
+
+const auto kTestTasksList = std::tuple_cat(
+    ppc::util::AddFuncTask<KhruevAGaussJordanMPI, InType>(
+        kTestParam, PPC_SETTINGS_khruev_a_gauss_jordan),
+    ppc::util::AddFuncTask<KhruevAGaussJordanSEQ, InType>(
+        kTestParam, PPC_SETTINGS_khruev_a_gauss_jordan));
+
+const auto kGtestValues = ppc::util::ExpandToValues(kTestTasksList);
+
+const auto kPerfTestName =
+    KhruevARunFuncTestsProcesses::PrintFuncTestName<
+        KhruevARunFuncTestsProcesses>;
+
+INSTANTIATE_TEST_SUITE_P(
+    GaussJordanTests,
+    KhruevARunFuncTestsProcesses,
+    kGtestValues,
+    kPerfTestName);
+
+}  // namespace
 }  // namespace khruev_a_gauss_jordan
