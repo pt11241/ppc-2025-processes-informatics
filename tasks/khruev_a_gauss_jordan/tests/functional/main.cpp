aaacf4160604eed84b211794eab9d04281c4c175
brb484ul <pppphp2800@gmail.com>
2025-12-31 00:44:56 +0300

    test fix

--- tasks/khruev_a_gauss_jordan/tests/functional/main.cpp
+++ tasks/khruev_a_gauss_jordan/tests/functional/main.cpp
@@ -55,22 +55,72 @@ TEST_P(KhruevARunFuncTestsProcesses, GaussJordan) {
   ExecuteTest(GetParam());
 }
 
-/* Уменьшенный и изменённый набор тестов */
 const std::array<TestType, 7> kTestParam = {
-    std::make_tuple("simple_2x2", InType{{3, 1, 7}, {1, 2, 5}}, OutType{1.0, 2.0}),
+    // 1) 2x + y = 5
+    //    x + y = 4
+    //    => x = 1, y = 3
+    std::make_tuple(
+        "simple_2x2",
+        InType{{2, 1, 5},
+               {1, 1, 4}},
+        OutType{1.0, 3.0}),
 
-    std::make_tuple("row_swap_case", InType{{0, 4, 8}, {2, 1, 5}}, OutType{2.0, 2.0}),
+    // 2) 0x + 3y = 6
+    //    2x + y = 5
+    //    => y = 2, x = 1.5
+    std::make_tuple(
+        "row_swap_case",
+        InType{{0, 3, 6},
+               {2, 1, 5}},
+        OutType{1.5, 2.0}),
 
-    std::make_tuple("identity_3x3", InType{{1, 0, 0, 4}, {0, 1, 0, 5}, {0, 0, 1, 6}}, OutType{4.0, 5.0, 6.0}),
+    // 3) x = 4, y = -1, z = 2
+    std::make_tuple(
+        "identity_3x3",
+        InType{{1, 0, 0, 4},
+               {0, 1, 0, -1},
+               {0, 0, 1, 2}},
+        OutType{4.0, -1.0, 2.0}),
 
-    std::make_tuple("diagonal_3x3", InType{{5, 0, 0, 10}, {0, 2, 0, 6}, {0, 0, 4, 8}}, OutType{2.0, 3.0, 2.0}),
+    // 4) 3x = 9
+    //    2y = 4
+    //    5z = 10
+    //    => x = 3, y = 2, z = 2
+    std::make_tuple(
+        "diagonal_3x3",
+        InType{{3, 0, 0, 9},
+               {0, 2, 0, 4},
+               {0, 0, 5, 10}},
+        OutType{3.0, 2.0, 2.0}),
 
-    std::make_tuple("fractional_coeffs", InType{{0.2, 0.4, 1.2}, {0.6, 0.2, 1.0}}, OutType{1.0, 2.0}),
+    // 5) 0.5x + 0.5y = 2
+    //    0.5x - 0.5y = 0
+    //    => x = 2, y = 2
+    std::make_tuple(
+        "fractional_coeffs",
+        InType{{0.5, 0.5, 2},
+               {0.5, -0.5, 0}},
+        OutType{2.0, 2.0}),
 
-    std::make_tuple("single_equation", InType{{5, 20}}, OutType{4.0}),
+    // 6) 7x = 21
+    //    => x = 3
+    std::make_tuple(
+        "single_equation",
+        InType{{7, 21}},
+        OutType{3.0}),
 
-    std::make_tuple("upper_triangular", InType{{2, 1, 1, 9}, {0, 1, 2, 8}, {0, 0, 2, 4}}, OutType{2.0, 2.0, 2.0}),
+    // 7) x + y + z = 6
+    //    y + z = 5
+    //    z = 3
+    //    => x = 1, y = 2, z = 3
+    std::make_tuple(
+        "upper_triangular",
+        InType{{1, 1, 1, 6},
+               {0, 1, 1, 5},
+               {0, 0, 1, 3}},
+        OutType{1.0, 2.0, 3.0}),
 };
+
 
 const auto kTestTasksList = std::tuple_cat(
     ppc::util::AddFuncTask<KhruevAGaussJordanMPI, InType>(kTestParam, PPC_SETTINGS_khruev_a_gauss_jordan),
